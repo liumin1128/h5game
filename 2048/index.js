397d7111a168d56aea897ab114749d2b0619b629
liumin <min_liu@singaporeair.com.sg>
2021-11-11 13:23:26 +0800

    2048

--- 2048/index.js
+++ 2048/index.js
@@ -34,7 +34,6 @@ function updateBox(box, i, j) {
 //   for (let j = 0; j < data[i].length; j++) {
 //     // let box = document.createElement("div");
 //     // box.setAttribute("class", "box box_" + (i + "_" + j));
-//     console.log(`
 //       .box_${i}_${j} {
 //         left: ${i * 200 + 5 + "px"};
 //         top: ${j * 200 + 5 + "px"};
@@ -50,149 +49,13 @@ for (let i = 0; i < data.length; i++) {
       continue;
     }
     let box = document.createElement("div");
-    box.setAttribute("class", "box box_" + (j + "_" + i));
+    box.setAttribute("class", "box box_value_"+ data[i][j] +" box_" + (j + "_" + i));
     // box.style.left = i * 200 + 5 + "px";
     // box.style.top = j * 200 + 5 + "px";
     box.innerText = data[i][j];
     document.querySelector(".actor").appendChild(box);
   }
 }
-
-function compare(x, dd, direction) {
-  let a;
-  a = dd;
-
-  // for (let i = 0; i < a.length; i++) {
-  //   if (a[i] === 0) {
-  //     // 移动最近的一个数
-  //     for (let j = i + 1; j < a.length; j++) {
-  //       if (a[j] !== 0) {
-  //         a[i] = a[j];
-  //         a[j] = 0;
-
-  //         const boxJ = document.querySelector(".box_" + x + "_" + j);
-  //         boxJ.setAttribute("class", "box box_" + (x + "_" + i));
-  //         boxJ.style.top = i * 200 + 5 + "px";
-
-  //         console.log("移动最近的一个数");
-  //         break;
-  //       }
-  //     }
-
-  //     console.log(a);
-
-  //     // 如果没有，跳出查找
-  //     if (a[i] === 0) {
-  //       console.log("没有，跳出查找");
-  //       break;
-  //     }
-
-  //     // 找到同数，若找到相加并跳过当前循环
-  //     for (let j = i + 1; j < a.length; j++) {
-  //       if (a[j] === a[i]) {
-  //         a[i] += a[j];
-  //         a[j] = 0;
-
-  //         const boxJ = document.querySelector(".box_" + x + "_" + j);
-  //         boxJ.setAttribute("class", "box remove box_" + (i + "_" + j));
-  //         boxJ.style.top = i * 200 + 5 + "px";
-
-  //         const boxI = document.querySelector(".box_" + x + "_" + i);
-  //         boxI.innerText = a[i];
-
-  //         continue;
-  //       }
-  //     }
-  //   }
-
-  //   // 找到同数，若找到相加并跳过当前循环
-  //   for (let j = i + 1; j < a.length; j++) {
-  //     if (a[j] === a[i]) {
-  //       a[i] += a[j];
-  //       a[j] = 0;
-
-  //       const boxJ = document.querySelector(".box_" + x + "_" + j);
-  //       boxJ.setAttribute("class", "box remove box_" + (i + "_" + j));
-  //       boxJ.style.top = i * 200 + 5 + "px";
-
-  //       const boxI = document.querySelector(".box_" + x + "_" + i);
-  //       boxI.innerText = a[i];
-
-  //       continue;
-  //     }
-
-  //     if (a[j] !== 0) {
-  //       break;
-  //     }
-  //   }
-  // }
-
-  // return cloneDeep(a);
-}
-
-document.querySelector(".up").addEventListener("click", function () {
-  console.log("data");
-  console.log(data);
-
-  // let d = cloneDeep(data[0])
-  // console.log(d);
-  // let newD = compare(0, d);
-  // console.log("newD");
-  // console.log(newD);
-  // data[0] =newD
-
-  // let d = cloneDeep(data[0]);
-  // let newD = compare(0, d);
-  // console.log("newD");
-  // console.log(newD);
-  for (let i = 0; i < data.length; i++) {
-    let d = cloneDeep(data[i]);
-    let newD = compare(i, d);
-    data[i] = newD;
-  }
-
-  // console.log("data");
-  // console.log(data);
-  // document.querySelector(".box_0_1").remove()
-});
-
-// document.querySelector(".down").addEventListener("click", function () {
-//   console.log("data");
-//   console.log(data);
-
-//   // let d = cloneDeep(data[0]).reverse();
-//   // console.log(d);
-//   // let newD = compare2(0, d);
-//   // console.log("newD");
-//   // console.log(newD);
-//   // data[0] = newD.reverse()
-
-//   for (let i = 0; i < data.length; i++) {
-//     let d = cloneDeep(data[i]).reverse();
-//     let newD = compare(i, d);
-//     data[i] = newD.reverse()
-//   }
-
-//   // console.log("data");
-//   // console.log(data);
-//   // document.querySelector(".box_0_1").remove()
-// });
-
-// for (let i = 0; i < 4; i++) {
-//   let box = document.createElement("div");
-//   box.setAttribute("class", "box");
-//   box.style.left = (i % 4) * 200 + 5 + "px";
-//   box.style.top = Math.floor(i / 4) * 200 + 5 + "px";
-//   box.innerText = "2"
-//   document.querySelector(".actor").appendChild(box);
-// }
-
-// let data = [
-//   [2, 0, 4, 0],
-//   [0, 0, 2, 2],
-//   [2, 4, 4, 2],
-//   [0, 0, 0, 2],
-// ];
 
 function getCurrentBox(d, i, j) {
   if (d === 0) {
@@ -208,13 +71,45 @@ function getCurrentBox(d, i, j) {
 
 const sleep  =(t) => new Promise((resolve, reject) => {setTimeout(resolve, t)})
 
-async function test1() {
-  // const r = getCurrentBox(0, 0, 1);
-  // console.log("r");
-  // console.log(r);
-  await sleep(1000)
+async function newBox() {
+  const list = [];
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if(data[i][j] === 0) {
+        list.push([i, j])
+      }
+    }
+  }
+  if(list.length === 0) {
+    alert("failed")
+  } else {
+    let index = Math.floor(Math.random() * list.length, 0)
 
-  const d = 2;
+    const [x,y] = list[index] 
+
+    data[x][y] = 2
+    let box = document.createElement("div");
+    box.setAttribute("class", "box box_value_"+ 2 +" box_" + (y + "_" + x));
+    box.innerText = 2
+    document.querySelector(".actor").appendChild(box);
+  }
+}
+
+let lock = false
+
+async function move(d) {
+
+  console.log("data")
+  console.log(data)
+
+  if(lock) {
+    return
+  }
+
+  lock = true
+  // const r = getCurrentBox(0, 0, 1);
+  // await sleep(1000)
+
 
   for (let i = 0; i < WIDTH; i++) {
     for (let j = 0; j < HEIGHT; j++) {
@@ -226,7 +121,6 @@ async function test1() {
         const [x, y] = getCurrentBox(d, i, j);
         const dj = data[x][y]
 
-        // console.log(dj)
 
         if (dj === 0) {
           for (let k = j + 1; k < HEIGHT; k++) {
@@ -236,7 +130,7 @@ async function test1() {
             if (dk !== 0) {
 
               const obj = document.querySelector(".box_" + _y + "_" + _x);
-              obj.setAttribute("class", "box box_" + (y + "_" + x));
+              obj.setAttribute("class", "box box_value_"+ dk +" box_" + (y + "_" + x));
               
               data[x][y] = data[_x][_y];
               data[_x][_y] = 0
@@ -264,7 +158,7 @@ async function test1() {
             const obj = document.querySelector(".box_" + y + "_" + x);
             const obk = document.querySelector(".box_" + _y + "_" + _x);
             
-            obk.setAttribute("class", "box remove box_" + (y + "_" + x));
+            obk.setAttribute("class", "box box_value_"+ dk +" remove box_" + (y + "_" + x));
             
             setTimeout(() => {
               obk.remove()
@@ -274,6 +168,7 @@ async function test1() {
             data[_x][_y] = 0
 
             obj.innerText = data[x][y]
+            obj.setAttribute("class", "box box_value_"+ data[x][y] +" box_" + (y + "_" + x));
 
             // comparet1()
 
@@ -286,8 +181,56 @@ async function test1() {
       comparet1()
    
     }
-    console.log("========");
   }
+
+  await sleep(500)
+  await newBox()
+
+  lock = false
+
+  console.log(data)
 }
 
-// test1();
+// move();
+
+document.querySelector(".up").addEventListener("click", function () {
+  move(0)
+})
+
+
+document.querySelector(".right").addEventListener("click", function () {
+  move(1)
+})
+
+document.querySelector(".down").addEventListener("click", function () {
+  move(2)
+})
+
+document.querySelector(".left").addEventListener("click", function () {
+  move(3)
+})
+
+document.body.addEventListener('keydown', (e) => {
+  switch (e.keyCode) {
+    case 38: {
+      move(0)
+      break;
+    }
+
+    case 39: {
+      move(1)
+      break;
+    }
+
+    case 40: {
+      move(2)
+      break;
+    }
+
+    case 37: {
+      move(3)
+      break;
+    }
+    
+  }
+ });
